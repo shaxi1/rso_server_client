5026c897d8cbb3fbaa59481c847ce52a5a9e9f20
Marek Szulc <235991@edu.p.lodz.pl>
2023-03-15 13:21:47 +0100

    Add prototypes for server_client_handler

--- server_client_handler.h
+++ server_client_handler.h
@@ -1,9 +1,32 @@
 #ifndef RSO_SERVER_CLIENT_SERVER_CLIENT_HANDLER_H
 #define RSO_SERVER_CLIENT_SERVER_CLIENT_HANDLER_H
 
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
 #include "communication_common.h"
 #include "server_parse_query.h"
 
-void server_write();
+struct client_t {
+    int socket_fd;
+    struct sockaddr_in client_address;
+};
+
+struct client_list_t {
+    int capacity;
+    int clients_connected;
+    struct client_t *clients;
+//    struct pthread_t
+};
+
+extern struct client_t *client_list;
+
+void initialize_server();
+void destroy_server();
+
+void remove_client();
+void add_client();
+
 
 #endif //RSO_SERVER_CLIENT_SERVER_CLIENT_HANDLER_H
